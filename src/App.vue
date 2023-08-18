b05697d3efbbe3e496f834b1534beee90a81b30c
poohinajar@gmail.com <poohinajar@gmail.com>
2023-08-18 17:17:19 +0900

    top버튼 추가

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,32 @@
     :sideState="sideState" 
     :aria-hidden="modalState"
   />
-  <nav class="gnb" :aria-hidden="modalState||sideState">
-    <h2 class="offscreen">주 메뉴</h2>
-    <ul class="list_gnb">
-      <li v-for="(item, i) in gnb" :key="i"><a href="#none" class="link_gnb">{{item}}</a></li>
-    </ul>
-  </nav>
-  <div class="area_sort" :aria-hidden="modalState||sideState">
-    <button type="button" class="btn" :class="sortState ? 'on' : ''" v-on:click="sortPrice()" :aria-pressed="sortState">낮은 가격순</button>
-    <button type="button" class="btn" :class="sortReverseState ? 'on' : ''" v-on:click="sortPriceReverse()" :aria-pressed="!sortState">높은 가격순</button>
-    <!-- <button type="button" class="btn" v-on:click="sortName()">이름순</button> -->
-  </div>
   <main id="main" :aria-hidden="modalState||sideState">
-    <h2 class="offscreen">매물 목록</h2>
-    <ul class="list_cards">
-      <CardItem 
-        :item = "datas[i]"
-        :key  = "item" 
-        :data = "datas"
-        :class= "{'sale': datas[i].sale}"
-        v-for = "(item, i) in datas" 
-        @openLayer = "openLayer"
-      />
-    </ul>
+    <div class="area_sort" :aria-hidden="modalState||sideState">
+      <button type="button" class="btn" :class="sortState ? 'on' : ''" v-on:click="sortPrice()" :aria-pressed="sortState">낮은 가격순</button>
+      <button type="button" class="btn" :class="sortReverseState ? 'on' : ''" v-on:click="sortPriceReverse()" :aria-pressed="!sortState">높은 가격순</button>
+      <!-- <button type="button" class="btn" v-on:click="sortName()">이름순</button> -->
+    </div>
+    <div class="area_cards">
+      <h2 class="offscreen">매물 목록</h2>
+      <ul class="list_cards">
+        <CardItem 
+          :item = "datas[i]"
+          :key  = "item" 
+          :data = "datas"
+          :class= "{'sale': datas[i].sale}"
+          v-for = "(item, i) in datas" 
+          @openLayer = "openLayer"
+        />
+      </ul>
+    </div>
   </main>
+  <button type="button" :class="['btn_top', {go_top: isTop}]" @click="scrollToTop()"><span class="offscreen">처음으로</span></button>
+  <footer class="footer">
+    <p class="copyright">© BANGBANB Co.,Ltd. All Rights Reserved.</p>
+    <a class="policy" href="#none">개인정보 처리방침</a>
+    <p class="caution">일부 상품의 경우 주식회사 방방 통신판매의 당사자가 아닌 통신판매중개자로서 매물, 매물정보, 거래에 대한 책임이 제한될 수 있으므로, 각 매물 페이지에서 구체적인 내용을 확인하시기 바랍니다.</p>
+  </footer>
   <ModalLayer 
     :datas = "datas" 
     :selectedId = "selectedId" 
@@ -51,13 +53,13 @@ export default {
     return{
       datas : data,
       menu : ['home', 'shop', 'about'],
-      gnb : ['home', 'shop', 'about'],
       like : [0, 0, 0],
       modalState : false,
       sideState : false,
       sortState : false,
       sortReverseState : false,
-      selectedId : 0
+      selectedId : 0,
+      isTop : false
     }
   },
   methods : {
@@ -83,12 +85,26 @@ export default {
       this.modalState = true;
       this.selectedId = idx;
       // this.$refs.modalFocus.focus();
+    },
+    scrollToTop(){
+      window.scrollTo(0,0);
     }
     // sortName(){
     //   this.datas.sort(function(a,b){
     //     return a.title -b.title;
     //   });
     // }
+  },
+  mounted(){
+    const that = this;
+    window.addEventListener('scroll', () => {
+        let scrollPos = window.scrollY;
+        if(scrollPos >= 100){
+            that.isTop = true;
+        } else {
+            that.isTop = false;
+        }
+    })
   },
   components: {
     Header,
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,12 @@
       <a href="#none" class="btn_gift"><span class="offscreen">선물함</span></a>
       <button type="button" class="btn_menu" v-on:click="$emit('openSnb')"  :aria-haspopup="sideState" :aria-expanded="sideState" aria-controls="wrap_snb"><span class="offscreen">마이페이지 메뉴</span></button>
     </div>
+    <nav class="gnb sticky" :aria-hidden="modalState||sideState">
+      <h2 class="offscreen">주 메뉴</h2>
+      <ul class="list_gnb">
+        <li v-for="(item, i) in gnb" :key="i"><a href="#none" class="link_gnb">{{item}}</a></li>
+      </ul>
+    </nav>
     <div id="wrap_snb" class="wrap_snb" v-if="sideState">
       <div class="area_profile">
         <span class="thumb_profile">
@@ -16,7 +22,7 @@
         </span>
         <span class="info_profile">
           <strong class="txt_name">이혜빈</strong>
-          <span class="txt_email">inarainbow@naver.com</span>
+          <span class="txt_email">inarainbow@naver.com inarainbow@naver.com</span>
         </span>
       </div>
       <nav class="snb">
@@ -39,7 +45,8 @@ export default {
   name: 'HeaderComp',
   data(){
     return{
-      snb : ['나의 활동', '개인정보 수정', '개인정보 이용내역', '설정']
+      snb : ['나의 활동', '개인정보 수정', '개인정보 이용내역', '설정'],
+      gnb : ['home', 'shop', 'about'],
     }
   },
   props : {
